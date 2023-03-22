31cdb96a847b77fbeee6c7bcbf17e91d8a86bbd5
Nikodem Wicon - Nicode.pl <107640395+Nixon4056@users.noreply.github.com>
2023-03-22 09:16:51 +0100

    Update vault-server-script.sh

--- vault-server-script.sh
+++ vault-server-script.sh
@@ -4,4 +4,3 @@ touch /var/log/vault_audit.log
 touch /var/log/vault_audit.log
 chmod 644 /var/log/vault_audit.log 
 chown vault:vault /var/log/vault_audit.log
-vault audit enable file file_path=/var/log/vault_audit.log
