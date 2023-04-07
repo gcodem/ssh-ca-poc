78af8cc9d8d7fcd6a920607ff0960be2da9a94dc
Nikodem <107640395+Nixon4056@users.noreply.github.com>
2023-04-07 18:17:46 +0200

    terraform fmt

--- provider.tf
+++ provider.tf
@@ -4,22 +4,21 @@ provider "google" {
   project     = var.project_id
   region      = var.region
   zone        = var.zone
-
 }
 
 module "servers" {
-  source = "./servers"
-  ssh_key = var.ssh_key
+  source   = "./servers"
+  ssh_key  = var.ssh_key
   vm_count = 2
 }
 
 module "vault" {
-  source                       = "terraform-google-modules/vault/google"
-  project_id                   = var.project_id
-  region                       = var.region
-  kms_keyring                  = var.kms_keyring
-  kms_crypto_key               = var.kms_crypto_key
-  user_startup_script          = "vault-server-script.sh"
+  source              = "terraform-google-modules/vault/google"
+  project_id          = var.project_id
+  region              = var.region
+  kms_keyring         = var.kms_keyring
+  kms_crypto_key      = var.kms_crypto_key
+  user_startup_script = "vault-server-script.sh"
 }
 
 output "vault_addr" {
